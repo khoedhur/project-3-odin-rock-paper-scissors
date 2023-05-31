6420f0488d98df8c959ed93d0c7bc6ed325f1ad7
Khoe Dhur <97301263+khoedhur@users.noreply.github.com>
2023-05-30 19:07:33 -0700

    Edit rps.js
    Add logic for player winning against computer

--- rps.js
+++ rps.js
@@ -1,1 +1,37 @@
-console.log("hello!");
+// This function will return a randomized choice for the "computer" player, either rock, paper or scissors
+function getComputerChoice() {
+    // put the 3 options in an array
+    const options = ["rock", "paper", "scissors"];
+    // return a random choice from the array
+    let choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+
+//This function will take the user's choice, then compare it to the computer's choice, then determine a winner or tie.
+//function game(playerSelection, computerSelection) {
+    // Ask for user input store it in variable
+    // get computer choice
+    // if player is rock and computer is paper OR if player is paper and computer is rock OR if player is scissors and computer is paper 
+    // player wins
+
+    // else if player is rock and computer is paper OR player is paper and computer is
+//}
+
+function playRound(playerSelection, computerSelection) {
+    // if player is rock and computer is paper OR if player is paper and computer is rock OR if player is scissors and computer is paper 
+    
+    let player = playerSelection.toUpperCase();
+    let computer = computerSelection.toUpperCase();
+    if (player === "ROCK" && computer === "SCISSORS" ||
+        player === "PAPER" &&  computer === "ROCK" ||
+        player === "SCISSORS" && computer === "PAPER") {
+            // player wins
+            console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }
+}
+
+const playerSelection = "rock";
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
