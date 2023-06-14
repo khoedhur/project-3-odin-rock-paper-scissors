c2807f4e3d446d0058c67b4e97466d4c4a7303f4
Khoe Dhur <97301263+khoedhur@users.noreply.github.com>
2023-06-14 14:36:23 -0700

    Edit rps.js
    Add helper function to get player choice

--- rps.js
+++ rps.js
@@ -1,11 +1,40 @@
+// TODO: KEEP TRACK OF WHO WINS AFTER 5 ROUNDS (BEST OF 3 TECHNICALLY)
+// TODO: FIX RETURN VALUE OF PLAYROUND() TO A SCORE?
+
+// TODO: CALCULATE WINNER BASED ON NUMBERS OF WINS
+//       if player wins, return 1
+//       if player loses, return -1
+//       variable with final score, should be >= 1 or 
+
 /**
  * Randomly selects rock, paper, or scissors from an array that will be the computer's choice 
- * @returns {String} The computer's choice which is rock, paper, or scissors
+ * @returns {String} The computer's choice of either rock, paper, or scissors
  */
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
-    let choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
+    let computerChoice = options[Math.floor(Math.random() * options.length)];
+    return computerChoice;
+}
+
+
+/**
+ * This function will prompt the user to input 'rock', 'paper', or 'scissors'
+ * It will convert it to lowercase
+ * If the user did input a correct option, it will re-prompt the user until they do
+ * @returns The player's choice of either rock, paper, scissors
+ */
+function getPlayerChoice() {
+    let playerChoice;
+    let isTyping = true;
+    while (isTyping) {
+        playerChoice = prompt("Rock, Paper, or Scissors").toLocaleLowerCase();
+        if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+            break;
+        } else {
+            console.log("Invalid input!");
+        }
+    }
+    return playerChoice;
 }
 
 
@@ -16,47 +45,19 @@ function getComputerChoice() {
  * @returns {String}                     Whether the player wins, loses, or ties to the computer
  */
 function playRound() {
-    console.log("This is rock, paper, scissors game against a computer \n");
-    console.log("A winner wll be determined after five rounds.");
-
-    // FIX LOGIC ERROR: if there is a tie, the game should re-prompt the user again, and keep playing until there is a winner
-    // while the user has not put in a valid input (rock paper scissors)
-    // or if the user and computer has tied
-    let isTyping = true;
-    while (isTyping) {
-        // Get a new computer choice every time
-        let computerSelection = getComputerChoice();
-        //keep prompting the user until they enter rock, paper, or scissors
-        let computer = computerSelection.toLocaleLowerCase();
-        let playerSelection = prompt("Please type 'rock', 'paper', or 'scissors`");
-        //convert to lowercase to cover caps
-        let player = playerSelection.toLocaleLowerCase();
-        // if player enters valid response
-        if (player === "rock" || player === "paper" || player === "scissors") {
-            console.log(player);
-            console.log(computer);
-
-            if (player === "rock" && computer === "scissors" ||
-                player === "paper" &&  computer === "rock" ||
-                player === "scissors" && computer === "paper") {
-                    return alert(`You win! ${playerSelection} beats ${computerSelection}!`);
-            } else if (player === "rock" && computer === "" ||
-                       player === "PAPER" &&  computer === "SCISSORS" ||
-                       player === "SCISSORS" && computer === "ROCK") {
-                    return alert(`You lose! ${playerSelection} loses to ${computerSelection}!`);
-            }
-        } 
-        else {
-            console.log("Invalid input! Please make sure you type a valid response!");
-        }
-        // if the computer and player both have the same pick
-        if (player === computer) {
-            console.log(`You and the computer both picked ${playerSelection}! No winner!`);
-        }
-    }
-
+    // Get player and computer choice
+    let player = getPlayerChoice();
+    let computer = getComputerChoice();
     
-}
+    //DELETE LATER
+    console.log(player);
+    console.log(computer);
+
+    // compare player and computer choices
+    // tie
+    // player rock vs computer scissors 
+
+}   
 
 
 /**
@@ -64,12 +65,17 @@ function playRound() {
  * @returns Whether the player won or lost the game after 5 rounds
  */
 function game() {
-    for (let i = 0; i < 5; i++) {
+    // intro Message
+    console.log("This is rock, paper, scissors game against a computer \n");
+    console.log("A winner wll be determined after five rounds.");
+
+   /*  for (let i = 0; i < 5; i++) {
         playRound();
     }   
-    return;
+    return; */
+    playRound();
 }
 
-// TODO: KEEP TRACK OF WHO WINS AFTER 5 ROUNDS (BEST OF 3 TECHNICALLY)
+
 
 game();
